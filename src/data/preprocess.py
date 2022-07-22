6ef1a9fe7d531ddf3daecf96c92f4aeeab7bdba6
dimdasci <dimds@fastmail.com>
2022-07-22 23:38:19 +0400

    Добавлена функция нормализации слов

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -2,6 +2,8 @@ import re
 import pandas as pd
 import os
 
+import pymorphy2
+
 from nltk.tokenize import word_tokenize
 from nltk import download
 from nltk.corpus import stopwords
@@ -16,25 +18,39 @@ IS_ALPHA_RE = re.compile('[\w-]{2,}')
 STOPWORDS = stopwords.words('russian')
 SYM_SPELL = SymSpell(max_dictionary_edit_distance=3, prefix_length=7)
 
-DICTIONARY_PATH = ('/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-2]) 
-                   + '/models/symspell/professions.txt')
+BASE_PATH = '/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-2])
+DICTIONARY_PATH = BASE_PATH + '/models/symspell/professions.txt'
+ABBREVIATIONS_PATH = BASE_PATH + '/datasets/external/abbreviation.csv'
 
 SYM_SPELL.load_dictionary(DICTIONARY_PATH, 0, 1)
 
+MA = pymorphy2.MorphAnalyzer()
 
 def tokenize_drop_punkt(sentence: str) -> list:
     '''Разбивает предложение на токены без знаков пунктуации
     '''
     return [w.lower() for w in word_tokenize(sentence, language='russian') if IS_ALPHA_RE.match(w)]
 
-def drop_stopwords(words: list) -> list:
+def drop_stopwords(words: list, extra_stop_words: list = []) -> list:
     '''Удаляет стоп-слова из списка слов
     '''
-    return [w for w in words if w not in STOPWORDS]
+    sw = STOPWORDS + extra_stop_words
+    return [w for w in words if w not in sw]
+
+def normalize_tokens(tokens: list) -> list:
+    '''Оставляет среди токенов только существительные и прилагательные в нормальной форме
+    '''
+    words = []
+    for word in tokens:
+        p = MA.parse(word)[0]
+        # для слесарь у pymorh на первом месте глагол слесарить, добавим исключение
+        if word == 'слесарь' or 'NOUN' in p.tag or 'ADJF' in p.tag:
+            words.append(p.normal_form)
+    return words  
 
 def read_abbreviations_dictionary(filename: str='../datasets/external/abbreviation.csv') -> dict:
     '''Считывает словарь из CSV файла и преобразует в dict'''
-    data = pd.read_csv('../datasets/external/abbreviation.csv', sep=';')
+    data = pd.read_csv(ABBREVIATIONS_PATH, sep=';')
     return dict(zip(data.abbreviation.to_list(), 
                     data.meaning.to_list()))
 
@@ -48,7 +64,7 @@ def unfold_abbreviation(words: list, abbreviations: dict) -> list:
                     
     return unfolded_words
 
-def spell_check(words: list) -> tuple:
+def spell_check(words: list, tolower=True) -> tuple:
     '''Заменяет опечатки в переданных словах.
     Возвращает:
         список исправленных слов
@@ -58,7 +74,10 @@ def spell_check(words: list) -> tuple:
     corrected_words = []
 
     for word in words:
-        corrected = word
+        if tolower:
+            word = word.lower()
+            
+        corrected = word 
 
         if len(word) > 3:
             suggestions = SYM_SPELL.lookup(word, Verbosity.CLOSEST, include_unknown=True)
