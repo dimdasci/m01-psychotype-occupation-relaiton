73f4e3be4eb0f94910b6b861fbb5ff024332848c
dimdasci <dimds@fastmail.com>
2022-07-24 18:56:04 +0400

    Добавлен импорт функций

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,6 @@ from src.data.preprocess import tokenize_drop_punkt
 from src.data.preprocess import drop_stopwords
 from src.data.preprocess import read_abbreviations_dictionary
 from src.data.preprocess import unfold_abbreviation
+from src.data.preprocess import normalize_tokens
+from src.data.preprocess import read_stopwords_dictionary
+from src.data.preprocess import spell_check
